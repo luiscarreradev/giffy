9dcfce47ef1de5725969c67015f29bf1ef332c0f
Luis Carrera <luiscarreramv@gmail.com>
2022-05-04 02:24:40 -0600

    Agregue fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 
-const GIFS = ['https://media.giphy.com/media/iDJQRjTCenF7A4BRyU/giphy.gif', 'https://media.giphy.com/media/dTJd5ygpxkzWo/giphy.gif']
-
-const DIFEREENT_GIFS = ['https://media.giphy.com/media/gfGvtlxqgYKIEqSEdE/giphy.gif', 'https://media.giphy.com/media/WpxeQuE1hfvLow9Ir3/giphy.gif']
+const API = 'https://api.giphy.com/v1/gifs/search?api_key=cFbaH37V7Dg4tm47pc5a8pVIaqG3yjsE&q=panda&limit=10&offset=0&rating=g&lang=en'
 
 function App() {
-    const [gifs, setGifs] = useState(GIFS)
+    const [gifs, setGifs] = useState([])
 
     useEffect(() => {
-      console.log('Renderizado!');
-    })
+      fetch(API)
+      .then(res => res.json())
+      .then(response => {
+          const {data} = response
+          if(Array.isArray){
+            const gifs = data.map(image => image.images.downsized_medium.url)
+            console.log(gifs);
+            setGifs(gifs)
+          }
+        })
+    }, [])
 
     return (
         <div className="App">
             <section className="App-content">
                 {
-                    gifs.map(singleGif => <img src={singleGif} />)
+                    gifs.map(singleGif => <img src={singleGif} alt="an gift"/>)
                 }
-                <button onClick={() => setGifs(DIFEREENT_GIFS)}>Cambiar Gifs</button>
             </section>
         </div>
     );

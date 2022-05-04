b62982db117dc6bdfe39f92da3d431404a26405c
Luis Carrera <luiscarreramv@gmail.com>
2022-05-04 01:30:13 -0600

    Creating first components in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+const GIFS = ['https://media.giphy.com/media/iDJQRjTCenF7A4BRyU/giphy.gif', 'https://media.giphy.com/media/dTJd5ygpxkzWo/giphy.gif']
+
+const DIFEREENT_GIFS = ['https://media.giphy.com/media/gfGvtlxqgYKIEqSEdE/giphy.gif', 'https://media.giphy.com/media/WpxeQuE1hfvLow9Ir3/giphy.gif']
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [gifs, setGifs] = useState(GIFS)
+
+    useEffect(() => {
+      console.log('Renderizado!');
+    })
+
+    return (
+        <div className="App">
+            <section className="App-content">
+                {
+                    gifs.map(singleGif => <img src={singleGif} />)
+                }
+                <button onClick={() => setGifs(DIFEREENT_GIFS)}>Cambiar Gifs</button>
+            </section>
+        </div>
+    );
 }
 
 export default App;
